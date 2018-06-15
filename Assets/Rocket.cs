d14d53834332448669cc34461cc940e25023478b
Rey.Perez <ichibanrei@gmail.com>
2018-06-15 14:43:39 -0700

    Build 1.0

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] float deathTime = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip explodeSound;
+    [SerializeField] AudioClip successSound;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem explodeParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    [Range(2, 5)] [SerializeField] int startingLives;
+
+    [SerializeField] bool isDebugMode = false;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
     // Use this for initialization
     void Start ()
@@ -14,28 +32,67 @@ public class Rocket : MonoBehaviour {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update ()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        if(Debug.isDebugBuild)
+        {
+            RespondToDebugKeys();
+        }
+        Exit();
 	}
 
-        private void Thrust()
+    private void RespondToDebugKeys()
     {
-        if (Input.GetKey(KeyCode.Space))
-        { 
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            PlayThrusterSound(false);
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.L)) // Advance level for debug
         {
-            PlayThrusterSound(true);
+            LoadNextLevel();
+        }
+        else if(Input.GetKeyDown(KeyCode.C)) // disable collision for debug
+        {
+            isDebugMode = true;
+            print("Debug mode enabled");
         }
     }
 
-    private void Rotate()
+    private void Exit()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        { 
+            ApplyThrust();            
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true; // take manual control of rotation
         float rotationThisFrame = rcsThrust * Time.deltaTime;
@@ -52,33 +109,58 @@ public class Rocket : MonoBehaviour {
         rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 
-    private void PlayThrusterSound(bool isThrustPlaying)
-    {
-        if (!audioSource.isPlaying)
-        {
-            audioSource.Play();
-        }
-        else if (isThrustPlaying)
-        {
-            audioSource.Stop();
-        }
-           
-    }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive || isDebugMode) { return; }
+
         switch(collision.gameObject.tag)
         {
             case "Friendly":
                 print("OK");//remove
                 break;
-            case "Fuel":
-                print("refueled");
+            case "Finish":
+                SuccessProcess();
                 break;
             default:
-                print("You Died");
+                DeathProcess();
                 break;
-
         }
+    }
+
+    private void SuccessProcess()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        print("Load audio file " + successSound.name);
+        audioSource.PlayOneShot(successSound);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void DeathProcess()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(explodeSound, 100f);
+        mainEngineParticles.Stop();
+        explodeParticles.Play();
+        Invoke("LoadFirstLevel", deathTime);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
